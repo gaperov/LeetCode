f3ebd4abd7982da9417a34c1415e99368207810e
gaperov <112892634+gaperov@users.noreply.github.com>
2026-03-28 14:24:54 +0300

    Update plus_one.py

--- Python3/plus_one.py
+++ Python3/plus_one.py
@@ -9,5 +9,15 @@ class Solution:
             if i == 0 and digits[i] == 0:
                 digits.insert(0, 1)
                 return digits
-                
+
+'''class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+
+        for i in range(len(digits)-1, -1, -1):
+            
+            carry, digits[i] = divmod(digits[i]+carry, 10)
+
+        return [1]+digits if carry > 0 else digits'''
+
         
