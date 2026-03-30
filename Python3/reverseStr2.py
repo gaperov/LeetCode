270896ac4225f5b40a0eb8bd19604a345ad8c943
gaperov <112892634+gaperov@users.noreply.github.com>
2026-03-31 00:04:30 +0300

    Create reverseStr2.py

---
+++ Python3/reverseStr2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        for i in range(0, len(s), k*2):
+            l = i
+            r = l + k - 1
+            if r > len(s)-1:
+                r = len(s)-1
+            while l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return ''.join(s)
