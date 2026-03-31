fb9a5a021d926f6f2b816aa62df841168f4c4b5a
gaperov <112892634+gaperov@users.noreply.github.com>
2026-03-31 12:24:13 +0300

    Create removeElementssortedList.py

---
+++ Python3/removeElementssortedList.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+
+        cur = head
+        prev = dummy
+
+        while cur:
+            if cur.val == val:
+                prev.next = cur.next
+                cur = cur.next
+            else:
+                cur = cur.next
+                prev = prev.next
+        return dummy.next
+        
